patrickrgaffney/Stopwatch.app

commit 64cd8051bed1a4bf77b9a52d6392b62a45dfb524

FIX: Launch app with 0 laps, not 1
Previously, the app would launch (same for after a reset) with
1 empty lap. This has been fixed so that the first lap is not
added until the timer begins -- both for launching the app and
for after a reset.

Stopwatch/Stopwatch.swift
@@ -58,7 +58,6 @@ class Stopwatch {
     ///
     init(delegate: StopwatchDelegate) {
         self.delegate = delegate
-        laps.append("")
     }
     
     // *****************************************************************
@@ -67,8 +66,12 @@ class Stopwatch {
     
     /// Start the stopwatch's timer.
     func startTimer() {
-        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(timerHasTicked(timer:)), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.01, target: self,
+                                     selector: #selector(timerHasTicked(timer:)),
+                                     userInfo: nil, repeats: true)
         RunLoop.current.add(timer!, forMode: RunLoopMode.commonModes)
+        
+        laps.append("")
         
         /// Record the time at which we started this timer.
         timerStartTime = Date.timeIntervalSinceReferenceDate
@@ -95,7 +98,6 @@ class Stopwatch {
         lapSavedTime = 0
         lapStartTime = nil
         laps.removeAll()
-        laps.append("")
         delegate?.timerWasReset()
     }
     

commit 8881d346e1dd627e7a8fff0393436b780818da23

FIX: Add a lap only on first Start, not subsequent
This fixes a bug that was introduced in 64cd805. A new lap was
being created every time startTimer() was called. This should only
be the functionality on the *first* call (when the app first
launches and after a rest). Every time stopTimer() is called, then
startTimer() is called without a reset, the previously running lap
should update. No new lap should be added.

Stopwatch/Stopwatch.swift
@@ -71,7 +71,11 @@ class Stopwatch {
                                      userInfo: nil, repeats: true)
         RunLoop.current.add(timer!, forMode: RunLoopMode.commonModes)
         
-        laps.append("")
+        /// If this is the first lap, add it to the screen. Otherwise this
+        /// is done by the completeLap() function.
+        if laps.count == 0 {
+            laps.append("")
+        }
         
         /// Record the time at which we started this timer.
         timerStartTime = Date.timeIntervalSinceReferenceDate